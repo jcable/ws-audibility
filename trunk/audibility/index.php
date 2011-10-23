fd318f30109865805fdd5fd39deab5f4666b0ff1
jfbcable@gmail.com <jfbcable@gmail.com@54ba2ad6-5ae4-94d1-fdbd-ee339922a1c9>
2011-10-23 09:43:46 +0000

    most basic functionality, more still needed

---
+++ trunk/audibility/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<HTML>
+<HEAD>
+<TITLE>HF Audibility</TITLE>
+<meta http-equiv="REFRESH" content="0;url=/cgi-bin/trac.cgi">
+</HEAD>
+<BODY>
+You are being redirected to the trac home page.
+</BODY>
+</HTML>
