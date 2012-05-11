fc1fb0f146b3311eeeba5dd00d82166d466df198
jfbcable@gmail.com <jfbcable@gmail.com@54ba2ad6-5ae4-94d1-fdbd-ee339922a1c9>
2012-05-11 10:06:52 +0000

    checked HF schedule importing

--- trunk/audibility/upload_hf_schedule.php
+++ trunk/audibility/upload_hf_schedule.php
@@ -4,7 +4,19 @@
 <?php
 
   require_once('ciraf_lib.php');
+  require_once('report_template_functions.php');
 
+
+  // allow use with no parameters
+  $file = "import/hf.txt";
+
+  // allow use from the command line
+  if(count($argv)==2)
+  {
+    $file = $argv[1];
+  }
+
+  // allow use with a form
   if(array_key_exists('file', $_FILES))
   {
      $file = $_FILES["file"]["tmp_name"];
@@ -12,32 +24,31 @@
      `mv $file import/$filename`;
      $file = "import/$filename";
   }
-  else
-  {
-     $file = "import/hf.txt";
-  }
-try {
-    $dbh = new PDO('pgsql:host=localhost;dbname=wsdata', 'www', 'www');
-} catch (PDOException $e) {
-    print "Error!: " . $e->getMessage() . "<br/>";
-    die();
-}
+
+  $dbh = pdo_login('wsdata', 'PG_USER', 'PG_PASSWORD');
+
 try {  
-  $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
   if (($handle = fopen($file, "r")) !== FALSE) {
-    $dbh->beginTransaction();
     $dbh->exec('CREATE TEMPORARY TABLE import (LIKE prism_hf_schedule)');
-    while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+    $dbh->beginTransaction();
+    $sth = $dbh->prepare("insert into import (valid_from, valid_to, start, stop, days, service, frequency, bearing, power, station, antenna_array, target_area, cirafs, sender, smds_receiver_group) values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)");
+    $n=0;
+    while (($data = fgetcsv($handle, 1000, ",", '"')) !== FALSE) {
         $data[12] = cirafs2array($data[12]);
-        $query = "insert into import (valid_from, valid_to, start, stop, days, service, frequency, bearing, power, station, antenna_array, target_area, cirafs, sender, smds_receiver_group) values ('".implode("','", $data)."')";
-        $dbh->exec($query);
+	if($data[13]=='')
+		$data[13]=0;
+	$sth->execute($data);
+	$n++;
     }
     fclose($handle);
-    $dbh->exec('CREATE TEMPORARY TABLE r (f date, t date)');
-    $dbh->exec('INSERT INTO r SELECT min(valid_from), max(valid_from) FROM import');
-    $dbh->exec('DELETE FROM prism_hf_schedule USING r WHERE valid_from >= r.f AND valid_from <= r.t');
-    $dbh->exec('INSERT INTO prism_hf_schedule SELECT * FROM import');
+    print "read $n records from $file<br>\n";
+    $dbh->exec('CREATE TEMPORARY TABLE r (f date, t date, sender integer)');
+    $dbh->exec('INSERT INTO r SELECT min(valid_from), max(valid_from), sender FROM import GROUP BY sender');
+    $n = $dbh->exec('DELETE FROM prism_hf_schedule s USING r WHERE s.sender = r.sender AND valid_from >= r.f AND valid_from <= r.t');
+    print "deleted $n records from prism_hf_schedule<br>\n";
+    $n = $dbh->exec('INSERT INTO prism_hf_schedule SELECT * FROM import');
+    print "inserted $n records into prism_hf_schedule<br>\n";
     $dbh->commit();
   }
 } catch (Exception $e) {
